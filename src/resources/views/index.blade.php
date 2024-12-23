00ad9a00600fe2eb93342160cae5059598f78a9d
matsumoto kaoru <bc.rw0130@gmail.com>
2024-12-23 21:42:59 +0900

    viewの修正

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -11,6 +11,7 @@
     </div>
 </div>
 
+
 <div class="todo__content">
     <form class="create-form" action="/todos" method="post">
         @csrf
@@ -26,12 +27,13 @@
             <tr class="todo-tabule__row">
                 <th class="todo-table__header">Todo</th>
             </tr>
+            @foreach($todos as $todo)
             <tr class="todo-table__row">
                 <td class="todo-table__item">
                     <form class="update-form">
                         @csrf
                         <div class="update-form__item">
-                            <input class="update-form__item-input" type="text" name="content" value="test">
+                            <p class="update-form__item-input">{{ $todo['content'] }}</p>
                         </div>
                         <div class="update-form__button">
                             <button class="update-form__button-submit" type="submit">更新</button>
@@ -47,27 +49,7 @@
                     </form>
                 </td>
             </tr>
-            <tr class="todo-table__row">
-                <td class="todo-table__item">
-                    <form class="update-form">
-                        @csrf
-                        <div class="update-form__item">
-                            <input class="update-form__item-input" type="text" name="content" value="test2">
-                        </div>
-                        <div class="update-form__button">
-                            <button class="update-form__button-submit" type="submit">更新</button>
-                        </div>
-                    </form>
-                </td>
-                <td class="todo-table__item">
-                    <form class="delete-form">
-                        @csrf
-                        <div class="delete-form__button">
-                            <button class="delete-form__button-submit" type="submit">削除</button>
-                        </div>
-                    </form>
-                </td>
-            </tr>
+            @endforeach
         </table>
     </div>
 </div>
