63ea2acff052b500acfacd00031a614731ce9f87
matsumoto kaoru <bc.rw0130@gmail.com>
2024-12-23 21:04:13 +0900

    コントローラの修正

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -10,7 +10,8 @@ class TodoController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $todos = Todo::all();
+        return view('index', compact('todos'));
     }
 
     public function store(Request $request)
