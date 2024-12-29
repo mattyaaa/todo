c0d82dc345dcc7e01bf38d71b2e0b04213457a77
matsumoto kaoru <bc.rw0130@gmail.com>
2024-12-29 17:26:45 +0900

    バリデーションの実装

--- src/app/Http/Controllers/CategoryController.php
+++ src/app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Http\Requests\CategoryRequest;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -13,7 +14,7 @@ class CategoryController extends Controller
         return view('category', compact('categories'));
     }
     
-    public function store(Request $request)
+    public function store(CategoryRequest $request)
     {
         $category = $request->only('name');
         Category::create($category);
